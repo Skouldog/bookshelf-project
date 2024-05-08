45ac8445377ddb10413fc16477071d6462d58387
bruce <tomdavidbruce@gmail.com>
2024-05-08 14:17:49 +0200

    Start von dashboard

---
+++ bookshelf/bookshelf/src/main/java/com/example/bookshelf/LandingPageController.java
@@ -0,0 +1,17 @@
+package com.example.bookshelf;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LandingPageController {
+
+
+
+    @GetMapping("/")
+    public String dashboard() {
+
+        return "dashboard";
+    }
+}
