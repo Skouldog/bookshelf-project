ee1459aa0e5ebe800c89515b3396c93b24b96784
Skouldog <168722961+Skouldog@users.noreply.github.com>
2024-05-14 18:46:21 +0200

    Dashboard Änderungen und erstellen der anderen Websiten

--- bookshelf/bookshelf/src/main/java/com/example/bookshelf/LandingPageController.java
+++ bookshelf/bookshelf/src/main/java/com/example/bookshelf/LandingPageController.java
@@ -5,6 +5,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,24 +13,55 @@ import java.util.Map;
 @Controller
 public class LandingPageController {
 
-HashMap<String, book> allBooks = new HashMap<>();
+    private static final String RESULTS_PATH = "/getMappingResults";
 
-@PostConstruct
-public void init() {
-    book Harry = new book("Harry Potter","J.K Rowling",399,0);
-    allBooks.put("Harry Potter", Harry);
-    book Lord = new book("Lord of the Rings","KeinPlan",800,0);
-    allBooks.put("Lord of the Rings", Lord);
-    book Hitchhiker = new book("Hitchhikers Guide to the Galaxy","Douglas Adam",200,199);
-    allBooks.put("Hitchhiker Guide", Hitchhiker);
-    book Hunger = new book("The Hunger Games","Kein Plan",300,14);
-allBooks.put("The Hunger Games", Hunger);
+    HashMap<String, book> allBooks = new HashMap<>();
+    HashMap<String, book> myBooks = new HashMap<>();
 
-}
+    @PostConstruct
+    public void init() {
+        book Harry = new book("Harry Potter", "J.K Rowling", 399, 0);
+        allBooks.put("Harry Potter", Harry);
+        book Lord = new book("Lord of the Rings", "KeinPlan", 800, 0);
+        allBooks.put("Lord of the Rings", Lord);
+        book Hitchhiker = new book("Hitchhikers Guide to the Galaxy", "Douglas Adam", 200, 199);
+        allBooks.put("Hitchhiker Guide", Hitchhiker);
+        book Hunger = new book("The Hunger Games", "Kein Plan", 300, 14);
+        allBooks.put("The Hunger Games", Hunger);
+        book Thirst = new book("The Hunger Games", "Kein Plan", 300, 14);
+        allBooks.put("Thirt", Thirst);
 
-    @GetMapping("/")
+    }
+
+    @GetMapping(RESULTS_PATH)
+    public String getMappingResults(RedirectAttributes redirectAttributes, String name) {
+       if(allBooks.containsKey(name)) {
+           myBooks.put(name, allBooks.get(name));
+       }else {
+           redirectAttributes.addFlashAttribute("errorMessage", "Book not found");
+       }
+    return "redirect:/dashboard";
+    }
+
+
+
+
+    @GetMapping("/dashboard")
     public String dashboard(Model model) {
-model.addAttribute("books", allBooks.values());
+        model.addAttribute("books", myBooks.values());
         return "dashboard";
+    }
+
+
+    @GetMapping("/impressum")
+    public String impressum(Model model) {
+
+        return "impressum";
+    }
+
+    @GetMapping("/quotes")
+    public String quotes(Model model) {
+
+        return "quotes";
     }
 }
