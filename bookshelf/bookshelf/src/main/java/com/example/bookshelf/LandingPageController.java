2aa4890574d3c7e451abe5ccd274839a1fe98dac
bruce <tomdavidbruce@gmail.com>
2024-06-03 12:03:06 +0200

    reverted and put BookRepository into own directory
    (cherry picked from commit f70b6a1817e4e901486413a91f8a5ac53d32de3b)

--- bookshelf/bookshelf/src/main/java/com/example/bookshelf/LandingPageController.java
+++ bookshelf/bookshelf/src/main/java/com/example/bookshelf/LandingPageController.java
@@ -1,7 +1,7 @@
 package com.example.bookshelf;
 
 
-import jakarta.annotation.PostConstruct;
+import com.example.bookshelf.repos.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Controller
--- bookshelf/bookshelf/src/main/java/com/example/bookshelf/repos/BookRepository.java
+++ bookshelf/bookshelf/src/main/java/com/example/bookshelf/repos/BookRepository.java
@@ -1,6 +1,7 @@
-package com.example.bookshelf;
+package com.example.bookshelf.repos;
 
 
+import com.example.bookshelf.book;
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface BookRepository extends ListCrudRepository<book, Long> {
