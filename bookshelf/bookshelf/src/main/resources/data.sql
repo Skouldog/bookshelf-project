76533d9cdc2aa4d90e8032f2d37dc8f057a9eebf
Kabeljaeger <169591970+Kabeljaeger@users.noreply.github.com>
2024-06-05 12:09:39 +0200

    revert

--- bookshelf/bookshelf/src/main/resources/data.sql
+++ bookshelf/bookshelf/src/main/resources/data.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS BOOKS (
 
 
 INSERT INTO BOOKS (TITLE, AUTHOR, PAGES_TOTAL, PAGE_CURRENT) VALUES
-                                                          ('Harry Potter', 'J.K. Rowling', 399, 0),
+                                                          ('Harry Potter', 'J.K. Rowling', 400, 0),
                                                           ('Lord of the Rings', 'J.R.R. Tolkien', 800, 0),
                                                           ('Hitchhikers Guide to the Galaxy', 'Douglas Adams', 200, 199),
                                                             ('The Hunger Games', 'Suzanne Collins', 300, 14);
