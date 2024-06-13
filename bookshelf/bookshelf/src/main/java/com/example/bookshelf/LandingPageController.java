c4cccc8f0966985f717aff5f7e3f66e268cdca4d
bruce <tomdavidbruce@gmail.com>
2024-06-13 11:37:47 +0200

    Buch aus Livereload hinzufügen

--- bookshelf/bookshelf/src/main/java/com/example/bookshelf/LandingPageController.java
+++ bookshelf/bookshelf/src/main/java/com/example/bookshelf/LandingPageController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -19,21 +20,19 @@ public class LandingPageController {
     private static final String RESULTS_PATH = "/getMappingResults";
 
     @Autowired
-    private IAllBookRepository allBookRepository;
-
-    List<AllBooks> personalLibrary = new ArrayList<>();
-
+    private IAllBookRepository allBookRepository; // Interface für die Bücher
+    List<AllBooks> personalLibrary = new ArrayList<>();// DIe eigenen Bücher, Dashboard Bücher
 
 
     @GetMapping(RESULTS_PATH) //Search Querery um Bücher zu finden
     public String getMappingResults(RedirectAttributes redirectAttributes, String name) {
         AllBooks searchedBook = allBookRepository.findByTitle(name);
-        if (personalLibrary.contains(searchedBook)) {
+        if (personalLibrary.contains(searchedBook)) { //Duplicated verhindern
             redirectAttributes.addFlashAttribute("errorMessage", "Book is already in Library");
-        } else if (searchedBook != null && searchedBook.getTitle().equals(name)) {
+        } else if (searchedBook != null && searchedBook.getTitle().equals(name)) { // Buch finden und hinzufügen
             personalLibrary.add(allBookRepository.findByTitle(name));
             redirectAttributes.addFlashAttribute("errorMessage", "Book has been added");
-        } else {
+        } else { //error / buch nicht gefunden
             redirectAttributes.addFlashAttribute("errorMessage", "Book not found");
         }
         return "redirect:/dashboard";
@@ -80,6 +79,24 @@ public class LandingPageController {
         return books.stream()
                 .map(AllBooks::getTitle)
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/addBook")// Teil vom Live Reload, leitet auf die SUbpage weiter um das Buch zu adden
+    public String addBook(@RequestParam("title") String title, Model model) {
+        model.addAttribute("title", title);
+        return "addNewBook";
+    }
+
+    @PostMapping("/addNewBook") // Hier wird das Buch abgespeichert
+    public String addNewBook(@RequestParam("title") String title,
+                             @RequestParam("author") String author,
+                             @RequestParam("PagesCurrent") int pagesCurrent,
+                             @RequestParam("PagesTotal") int pagesTotal, Model model)
+    {
+        AllBooks newBook = new AllBooks(title, author, pagesTotal, pagesCurrent);
+        allBookRepository.save(newBook);
+        personalLibrary.add(allBookRepository.findByTitle(title));
+        return("redirect:/dashboard");
     }
 
 }
